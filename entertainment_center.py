88c9a67374fa6779feee2450108b6c91c32b9226
Ayoolaao <ao.abimbola@gmail.com>
2016-11-26 03:33:20 -0600

    q

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,3 +1,14 @@
 import media
 
-toy_story = media.Movie()
+toy_story = media.Movie("The Flash",
+                        "The story of a boy",
+                        "http://cdn-static.sidereel.com/tv_shows/57093/giant_2x/the-flash.jpg",
+                        "https://www.youtube.com/watch?v=f5gmEHbBPWs&t=5s")
+
+ayo_story = media.Movie("The Ayoola",
+                        "The story of a man",
+                        "http://cdn-static.sidereel.com/tv_shows/57093/giant_2x/the-flash.jpg",
+                        "https://www.youtube.com/watch?v=f5gmEHbBPWs&t=5s")
+
+print(toy_story.storyline)
+print(ayo_story.storyline)
