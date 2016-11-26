a778911017b3d3c6f6be412c0457ada3c7ea1411
Ayoolaao <ao.abimbola@gmail.com>
2016-11-26 13:03:37 -0600

    added methods

--- entertainment_center.py
+++ entertainment_center.py
@@ -10,5 +10,12 @@ ayo_story = media.Movie("The Ayoola",
                         "http://cdn-static.sidereel.com/tv_shows/57093/giant_2x/the-flash.jpg",
                         "https://www.youtube.com/watch?v=f5gmEHbBPWs&t=5s")
 
-print(toy_story.storyline)
+new_movie = media.Movie("Mr Eazi",
+                        "The eazzi man",
+                        "http://www.chartsinfrance.net/covers/aHR0cHM6Ly9pLnNjZG4uY28vaW1hZ2UvOWIxYjg2ZWJiNjVhMWYxOTU1NmQzYjBkMDI1ZmRmNGE5N2ZkNmFkOA==.jpg",
+                        "https://www.youtube.com/watch?v=vk_4yxkgjAI")
+
 print(ayo_story.storyline)
+ayo_story.show_trailer()
+
+new_movie.show_trailer()
